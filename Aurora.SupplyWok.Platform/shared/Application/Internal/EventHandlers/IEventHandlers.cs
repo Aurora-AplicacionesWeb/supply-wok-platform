b8ed23d167baaea6fe2730db88aff56afba23c8a
Marcelo-alt-lab <marcelocuadrosalt@gmail.com>
2026-06-04 20:27:09 -0500

    feat(shared): update .gitignore and add Application Layer.

---
+++ Aurora.SupplyWok.Platform/shared/Application/Internal/EventHandlers/IEventHandlers.cs
@@ -0,0 +1,8 @@
+﻿using Acme.Center.Platform.Shared.Domain.Model.Events;
+using Cortex.Mediator.Notifications;
+
+namespace Aurora.SupplyWok.Platform.Application.Model;
+
+public interface IEventHandler<in TEvent> : INotificationHandler<TEvent> where TEvent : IEvent
+{
+}
