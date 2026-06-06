988c2b679c772a1ad8312c8d20e2d6dc6cbd3831
Marcelo-alt-lab <marcelocuadrosalt@gmail.com>
2026-06-06 18:24:02 -0500

    feat(Sensor): finish the Domain Layer for the moment.

--- Aurora.SupplyWok.Platform/Iot/Domain/Model/Aggregate/SensorContent.cs
+++ Aurora.SupplyWok.Platform/Iot/Domain/Model/Aggregate/SensorContent.cs
@@ -2,7 +2,14 @@
 
 namespace Aurora.SupplyWok.Platform.Iot.Domain.Model.Aggregate;
 
+/// <summary>
+/// Partial class for the <see cref="Sensor"/> aggregate
+/// </summary>
 public partial class Sensor {
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Sensor"/> class with default values.
+    /// </summary>
     public Sensor()
     {
         Name = string.Empty;
---
+++ Aurora.SupplyWok.Platform/Iot/Domain/Repositories/ISensorRepository.cs
@@ -0,0 +1,18 @@
+﻿using Aurora.SupplyWok.Platform.Iot.Domain.Model.Aggregate;
+using Aurora.SupplyWok.Platform.Shared.Domain.Repositories;
+
+namespace Aurora.SupplyWok.Platform.Iot.Domain.Repositories;
+
+/// <summary>
+/// Represents the Sensor repository in the Supply Wok Platform.
+/// </summary>
+public interface ISensorRepository : IBaseRepository<Sensor>
+{
+    /// <summary>
+    /// Find a sensor by id
+    /// </summary>
+    /// <param name="id">The id of the sensor to search for</param>
+    /// <param name="cancellationToken">The cancellation token</param>
+    /// <returns>The <see cref="Sensor"/> if found, otherwise null</returns>
+    Task<Sensor?> GetSensorByIdAsync(int id, CancellationToken cancellationToken);
+}
