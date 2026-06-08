7aa7ac9812728d4c87f81424eabb11d5d3392876
Nounz27 <mathiasenrique.sanchez@gmail.com>
2026-06-08 12:10:23 -0500

    feat(tables): implement command and query services for table management

---
+++ Aurora.SupplyWok.Platform/Operations/Application/CommandServices/ITableCommandService.cs
@@ -0,0 +1,14 @@
+﻿using Aurora.SupplyWok.Platform.Operations.Domain.Model.Aggregate;
+using Aurora.SupplyWok.Platform.Operations.Domain.Model.Commands;
+using Aurora.SupplyWok.Platform.Shared.Application.Model;
+namespace Aurora.SupplyWok.Platform.Operations.Application.CommandServices;
+
+/// <summary>
+/// Table command service interface
+/// </summary>
+public interface ITableCommandService
+{
+    Task<Result<Table>> Handle(CreateTableCommand command, CancellationToken cancellationToken);
+    Task<Result<Table>> Handle(UpdateTableCommand command, CancellationToken cancellationToken);
+    Task<Result<bool>> Handle(DeleteTableCommand command, CancellationToken cancellationToken);
+}
