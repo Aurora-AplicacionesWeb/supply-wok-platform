1f2e8514498b416a9729beee506f8081a4efc8ae
Nounz27 <mathiasenrique.sanchez@gmail.com>
2026-06-08 12:07:25 -0500

    feat(tables): add operations context and repository for table management

---
+++ Aurora.SupplyWok.Platform/Operations/Infrastructure/Persistence/EntityFrameworkCore/Repositories/TableRepository.cs
@@ -0,0 +1,17 @@
+﻿using Aurora.SupplyWok.Platform.Operations.Domain.Model.Aggregate;
+using Aurora.SupplyWok.Platform.Operations.Domain.Repositories;
+using Aurora.SupplyWok.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+using Aurora.SupplyWok.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Aurora.SupplyWok.Platform.Operations.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+
+public class TableRepository(AppDbContext context) : BaseRepository<Table>(context), ITableRepository
+{
+    public async Task<Table?> GetTableByIdAsync(int id, CancellationToken cancellationToken)
+    {
+        return await Context.Set<Table>().FirstOrDefaultAsync(t => t.Id == id, cancellationToken);
+    }
+    
+}
