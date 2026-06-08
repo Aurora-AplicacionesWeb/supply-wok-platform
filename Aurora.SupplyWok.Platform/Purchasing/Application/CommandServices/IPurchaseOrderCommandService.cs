3fd812c9fda7707781db1c24094e81f9453ed890
ZaydAyasta <81496269+ZaydAyasta@users.noreply.github.com>
2026-06-07 23:56:06 -0500

    feat(purchasing): add purchase order application services

---
+++ Aurora.SupplyWok.Platform/Purchasing/Application/CommandServices/IPurchaseOrderCommandService.cs
@@ -0,0 +1,16 @@
+using Aurora.SupplyWok.Platform.Purchasing.Domain.Model.Aggregate;
+using Aurora.SupplyWok.Platform.Purchasing.Domain.Model.Commands;
+using Aurora.SupplyWok.Platform.Shared.Application.Model;
+
+namespace Aurora.SupplyWok.Platform.Purchasing.Application.CommandServices;
+
+public interface IPurchaseOrderCommandService
+{
+    Task<Result<PurchaseOrder>> Handle(CreatePurchaseOrderCommand command, CancellationToken cancellationToken);
+
+    Task<Result<PurchaseOrder>> Handle(UpdatePurchaseOrderCommand command, CancellationToken cancellationToken);
+
+    Task<Result<bool>> Handle(DeletePurchaseOrderCommand command, CancellationToken cancellationToken);
+
+    Task<Result<PurchaseOrder>> Handle(UpdatePurchaseOrderStatusCommand command, CancellationToken cancellationToken);
+}
