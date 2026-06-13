1a5ff2b38c015d04746a15f9d1dafb4c7dd87d03
Nounz27 <mathiasenrique.sanchez@gmail.com>
2026-06-13 15:55:16 -0500

    feat(supply): add supply commands and unit of measure enumeration

---
+++ Aurora.SupplyWok.Platform/Inventory/Domain/Model/Commands/CreateSupplyCommand.cs
@@ -0,0 +1,12 @@
+﻿using Aurora.SupplyWok.Platform.Inventory.Domain.Model.ValueObjects;
+namespace Aurora.SupplyWok.Platform.Inventory.Domain.Model.Commands;
+
+/// <summary>
+/// Command to create a new supply
+/// </summary>
+/// <param name="Name"></param>
+/// <param name="UnitOfMeasure"></param>
+/// <param name="CurrentStock"></param>
+/// <param name="MinimumStockLevel"></param>
+/// <param name="category"></param>
+public record CreateSupplyCommand(string Name, EUnitOfMeasure UnitOfMeasure, int CurrentStock, int MinimumStockLevel, string category);
---
+++ Aurora.SupplyWok.Platform/Inventory/Domain/Model/Commands/UpdateSupplyCommand.cs
@@ -0,0 +1,12 @@
+﻿using Aurora.SupplyWok.Platform.Inventory.Domain.Model.ValueObjects;
+namespace Aurora.SupplyWok.Platform.Inventory.Domain.Model.Commands;
+
+/// <summary>
+/// Command to update a supply item.
+/// </summary>
+/// <param name="Id"></param>
+/// <param name="Name"></param>
+/// <param name="UnitOfMeasure"></param>
+/// <param name="MinimumStockLevel"></param>
+/// <param name="category"></param>
+public record UpdateSupplyCommand(int Id, string Name, EUnitOfMeasure UnitOfMeasure, int MinimumStockLevel, string category);
