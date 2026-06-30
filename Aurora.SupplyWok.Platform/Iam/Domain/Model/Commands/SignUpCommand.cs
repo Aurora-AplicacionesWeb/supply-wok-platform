85a66e0aae6dd426990c6df64d71be8012a3a0a0
Marcelo-alt-lab <marcelocuadrosalt@gmail.com>
2026-06-30 16:00:41 -0500

    Feat: add Domain Layer in IAM bc.

---
+++ Aurora.SupplyWok.Platform/Iam/Domain/Model/Commands/SignUpCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace Aurora.SupplyWok.Platform.Iam.Domain.Model.Commands;
+
+/**
+ * <summary>
+ *     The sign up command
+ * </summary>
+ * <remarks>
+ *     This command object includes the Email and password to sign up
+ * </remarks>
+ */
+public record SignUpCommand(string Email, string Password);
