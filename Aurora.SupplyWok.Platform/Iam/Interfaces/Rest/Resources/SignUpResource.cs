684b9d6435cb408bfcdb28b8fd8a0ed024f624e4
MaxghZZ <milenkorvu@gmail.com>
2026-07-03 13:12:02 -0500

    feat: add Role to SignUpCommand and SignUpResource.

--- Aurora.SupplyWok.Platform/Iam/Interfaces/Rest/Resources/SignUpResource.cs
+++ Aurora.SupplyWok.Platform/Iam/Interfaces/Rest/Resources/SignUpResource.cs
@@ -1,3 +1,3 @@
 namespace Aurora.SupplyWok.Platform.Iam.Interfaces.Rest.Resources;
 
-public record SignUpResource(string Email, string Password);
+public record SignUpResource(string Email, string Password, string Role = "restaurant");
